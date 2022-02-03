2e71f279e24ac3afb8966e34d7834e3d6c703724
HarshaRani <ranishashi@gmail.com>
2022-02-04 00:18:30 +0530

    Update listfiles_run.py
    directory and sub-directory

--- listfiles_run.py
+++ listfiles_run.py
@@ -1,11 +1,28 @@
 #listing all the python files in the directory and running those files
 import os
 from subprocess import call
-directory = '/home/harsharani/moose/hrani_moose-example_cleanuppatch/snippets/'
-for filename in os.listdir(directory):
+directory = '/home/rani/moose-examples/tutorials/'
+listOfFiles = list()
+for (dirpath, dirnames, filenames) in os.walk(directory):
+    listOfFiles += [os.path.join(dirpath, file) for file in filenames]
+#print(" list ", listOfFiles)
+for filename in listOfFiles:
     if filename.endswith(".py"):
-        ts =  os.path.join(directory, filename)
-        call(["python3", ts])
-        continue
+        print("FN ",filename)
+        if filename not in ["multiscaleOneCompt.py"]:
+            ts =  os.path.join(directory, filename)
+            call(["python3", ts])
+            continue
     else:
         continue
+'''
+for filename in os.listdir(directory):
+    if filename.endswith(".py"):
+        print("FN ",filename)
+        if filename not in ["multiscaleOneCompt.py"]:
+            ts =  os.path.join(directory, filename)
+            call(["python3", ts])
+            continue
+    else:
+        continue
+'''
